6eb621d1e0d180587eee8cb40dd6e0725ea5db59
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-08-12 18:29:15 +0300

    test

--- src/main/java/com/epam/rd/contactbook/Contact.java
+++ src/main/java/com/epam/rd/contactbook/Contact.java
@@ -1,42 +1,217 @@
 package com.epam.rd.contactbook;
 
 public class Contact {
+    public String contactName;
+
+
+    int counterSocial, emailCounter, counterPhone, indexOfEmail = 2, indexOfSocial = 5;
+    public final ContactInfo[] contactInfoEntries = new ContactInfo[10];
+
+    String title;
+    String linkId;
+    String value;
+    private int code;
+    private int number;
 
     public Contact(String contactName) {
-        //Implement this method
+        if (contactName != null) {
+            this.contactName = contactName;
+            contactInfoEntries[0] = new NameContactInfo();
+        }
+    }
+
+    private class NameContactInfo implements ContactInfo {
+        public String title = "Name";
+
+
+
+
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String getValue() {
+            return contactName;
+        }
+
+    }
+
+    public static class Email implements ContactInfo {
+
+        private String title, value;
+
+        public Email(String title, String value) {
+            this.title = title;
+            this.value = value;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public static class Social implements ContactInfo {
+
+        private String title, value;
+
+
+        public Social(String title, String linkId) {
+            this.title = title;
+            this.value = linkId;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String getValue() {
+            return value;
+        }
     }
 
     public void rename(String newName) {
-        //Implement this method
+
+        if (newName != null && newNameIsEmpty(newName)) {
+            contactName = newName;
+
+        }
     }
 
+       public boolean newNameIsEmpty(String newName) {
+           if(newName.equals(" ")) {
+               return true;
+           }
+           return false;
+        }
+
+
     public Email addEmail(String localPart, String domain) {
-        //Implement this method
+       if(localPart != null && domain != null) {
+           String title = "Email";
+           String value = localPart + "@" + domain;
+           Contact.Email email = new Contact.Email(title, value);
+           if (emailCounter < 3 && indexOfEmail <= 4) {
+               contactInfoEntries[indexOfEmail++] = email;
+               emailCounter++;
+               return email;
+           }
+       }
+return null;
     }
 
 
     public Email addEpamEmail(String firstname, String lastname) {
-        //Implement this method
+        if(firstname != null && lastname != null) {
+            String title = "Epam Email";
+            String value = firstname + "_" + lastname + "@epam.com";
+            Contact.Email epamEmail = new Contact.Email(title, value) {
+                @Override
+                public String getTitle() {
+                    return title;
+                }
+
+                @Override
+                public String getValue() {
+                    return value;
+                }
+            };
+            if (emailCounter < 3 && indexOfEmail <= 4) {
+                contactInfoEntries[indexOfEmail++] = epamEmail;
+                emailCounter++;
+                return epamEmail;
+            }
+        }
+        return null;
     }
 
     public ContactInfo addPhoneNumber(int code, String number) {
-        //Implement this method
+
+        ContactInfo phoneNumber = new ContactInfo() {
+
+            @Override
+            public String getTitle() {
+                return "Tel";
+            }
+
+            @Override
+            public String getValue() {
+                return "+" + code + " " + number;
+            }
+        };
+        if (counterPhone < 1) {
+        counterPhone++;
+                return contactInfoEntries[1] = phoneNumber; }
+        return null;
     }
+
 
     public Social addTwitter(String twitterId) {
-        //Implement this method
+        if (twitterId != null) {
+            String title = "Twitter";
+            Contact.Social twitter = new Contact.Social(title, twitterId);
+            if(counterSocial < 5 && indexOfSocial <= 9) {
+                contactInfoEntries[indexOfSocial++] = twitter;
+                counterSocial++;
+                return twitter;
+            }
+        }
+        return null;
     }
 
+
     public Social addInstagram(String instagramId) {
-        //Implement this method
+        if (instagramId != null) {
+            String title = "Instagram";
+            Contact.Social instagram = new Contact.Social(title, instagramId);
+            if(counterSocial < 5 && indexOfSocial <= 9) {
+                contactInfoEntries[indexOfSocial++] = instagram;
+                counterSocial++;
+                return instagram;
+            }
+        }
+        return null;
+
+        /*public Social addInstagram(String instagramId) {
+            Social instagram = new Social() {
+                public String getTitle() {return "Instagram";}
+                public String getValue() {return instagramId;}};
+            int counter = 0;
+            if(counter <= socialCounter) {contactInfoEntries[counter++] = instagram; return instagram;}
+            return null;*/
     }
 
     public Social addSocialMedia(String title, String id) {
-        //Implement this method
+        if (title != null && id != null) {
+            Social socialMedia = new Social(title, id);
+            if (counterSocial < 5 && indexOfSocial <= 9) {
+                contactInfoEntries[indexOfSocial++] = socialMedia;
+                counterSocial++;
+                return socialMedia;
+            }
+        }
+        return null;
     }
 
     public ContactInfo[] getInfo() {
-        //Implement this method
+       int counterTotal = counterPhone + emailCounter + counterSocial + 1;
+       ContactInfo[] finalContactInfoEntries = new ContactInfo[counterTotal];
+       int i = 0;
+       for (ContactInfo value : contactInfoEntries) {
+           if(value != null) {
+               finalContactInfoEntries[i++] = value;
+           }
+       }
+       return finalContactInfoEntries;
     }
-
 }
--- src/main/java/com/epam/rd/contactbook/ContactInfo.java
+++ src/main/java/com/epam/rd/contactbook/ContactInfo.java
@@ -1,6 +1,8 @@
 package com.epam.rd.contactbook;
 
 public interface ContactInfo {
+
+
     String getTitle();
     String getValue();
 }
